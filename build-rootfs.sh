fce9814c943945477f05f968483ceafc5318ca61
João Pinto <lamego.pinto@gmail.com>
2023-08-04 05:31:32 +0200

    Use export instead of save

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+set -eu
+
+BASE_IMAGE=debian:12-slim
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 rm -f /tmp/debian_12_slim.tar.gz
 mkdir -p artifacts
-docker pull debian:12-slim
-docker save -o artifacts/debian_12_slim.tar debian:12-slim
+docker rm devex 2>/dev/null|| true
+
+docker run \
+    -v ${SCRIPT_DIR}:/build \
+    --name devex \
+    $BASE_IMAGE
+
+docker export -o artifacts/debian_12_slim.tar devex
+rm -f  artifacts/debian_12_slim.tar.gz
 gzip -9 artifacts/debian_12_slim.tar
 du -sh artifacts/debian_12_slim.tar.gz
 
