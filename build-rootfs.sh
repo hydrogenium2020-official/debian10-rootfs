225560b4323fefc3d6b3b61f1db1ea6c32273f2b
João Pinto <lamego.pinto@gmail.com>
2023-08-04 08:03:41 +0200

    Add devex user and wls.conf

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -9,13 +9,23 @@ rm -f /tmp/debian_12_slim.tar.gz
 mkdir -p artifacts
 docker rm devex 2>/dev/null|| true
 
+echo "Starting docker container to build rootfs"
 docker run \
     -v ${SCRIPT_DIR}:/build \
     --name devex \
     $BASE_IMAGE
-
+echo "Exporting the tar file"
 docker export -o artifacts/debian_12_slim.tar devex
+
+rm -rf /tmp/rootfs && mkdir -p /tmp/rootfs
+tar -C /tmp/rootfs -xf artifacts/debian_12_slim.tar
+
+echo "Adding the DevEx User"
+proot -b $(pwd) -r /tmp/rootfs useradd -m  devex
+cp -v etc/* /tmp/rootfs/etc
+
+echo "Rebuilding the tar file"
 rm -f  artifacts/debian_12_slim.tar.gz
-gzip -9 artifacts/debian_12_slim.tar
+tar -C /tmp/rootfs -czf artifacts/debian_12_slim.tar.gz .
 du -sh artifacts/debian_12_slim.tar.gz
 
