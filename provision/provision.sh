c87ba27b3cd620474fa901bf3ab36bdc8664099d
João Pinto <lamego.pinto@gmail.com>
2023-08-06 07:32:25 +0200

    Use requirements from the profile repo

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -eu
 
+apt update && apt -y install curl
+
 cd "$(dirname "$0")" 
 
-REQUIRE_PACKAGES=$(grep -vE '^\s*#' requirements.txt)
+# Crete a random temporary directory
+TMP_DIR=$(mktemp -d)
+
+LATEST_URL="https://github.com/KindOS-workspace/system-profile/releases/latest/download/debian12_requirements.txt"
+curl -sL $LATEST_URL -o $TMP_DIR/requirements.txt
+
+REQUIRE_PACKAGES=$(grep -vE '^\s*#' $TMP_DIR/requirements.txt)
 
 apt-get update
 apt-get install -y $REQUIRE_PACKAGES
